c270dfcbec45ac1dca21416b2677d584d0ac1d45
John Amiscaray <john_ernest_amiscaray@outlook.com>
2021-08-12 10:21:15 -0400

    removed all sensitive firebase info for demonstration purposes

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCE1EyckF_NRex0vdyMGS7Zx5jUD19dErk",
-    authDomain: "spring-angular-fcm-demo.firebaseapp.com",
-    projectId: "spring-angular-fcm-demo",
-    storageBucket: "spring-angular-fcm-demo.appspot.com",
-    messagingSenderId: "142109885118",
-    appId: "1:142109885118:web:df93d5e0cc6added01d4ef",
-    measurementId: "G-DVYFZC7TGZ"
+    apiKey: "your api key",
+    authDomain: "your auth domain",
+    projectId: "your project id",
+    storageBucket: "your storage bucket",
+    messagingSenderId: "your messaging sender id",
+    appId: "your app id",
+    measurementId: "your measurement id"
   }
 };
